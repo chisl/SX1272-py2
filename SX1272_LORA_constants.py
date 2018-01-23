ef151011efa1970792400a52884b83a47b6d80e6
chisl <info@chisl.io>
2018-01-23 05:49:20 +0100

    update

--- SX1272_LORA_constants.py
+++ SX1272_LORA_constants.py
@@ -6,10 +6,15 @@ __author__     = "ChISL"
 __copyright__  = "TBD"
 __credits__    = ["Semtech"]
 __license__    = "TBD"
-__version__    = "0.1"
+__version__    = "Version 0.1"
 __maintainer__ = "https://chisl.io"
 __email__      = "info@chisl.io"
 __status__     = "Test"
+
+#
+#   THIS FILE IS AUTOMATICALLY CREATED
+#    D O     N O T     M O D I F Y  !
+#
 
 class REG:
 	Fifo = 0
